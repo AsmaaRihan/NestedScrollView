4fe60e34a20553b876396a870ca8e728c769ea6e
Asmaa Rihan <36204984+AsmaaRihan@users.noreply.github.com>
2021-05-27 15:21:14 +0200

    code

---
+++ lib/MainPage.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:learningApp/SliverDetails.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+  TabController _controllerTab;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controllerTab = TabController(length: 2, vsync: this);
+    _controllerTab.addListener(() => {setState(() {})});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: NestedScrollView(
+            physics: NeverScrollableScrollPhysics(),
+            headerSliverBuilder: (context, isScrolled) {
+              return [
+                SliverAppBar(
+                  floating: true,
+                  pinned: false,
+                  snap: false,
+                  elevation: 50,
+                  backgroundColor: Colors.green,
+                  collapsedHeight: MediaQuery.of(context).size.height * 0.45,
+                  expandedHeight: MediaQuery.of(context).size.height * 0.45,
+                  flexibleSpace: FlexibleSpaceBar(), // ProfileView()
+                ),
+                SliverPersistentHeader(
+                  pinned: true,
+                  floating: true,
+                  delegate: SliverPersistentHeaderDelegateImpl(
+                    color: Colors.white,
+                    tabBar: tabBar(),
+                  ),
+                ),
+              ];
+            },
+            body: TabBarView(controller: _controllerTab, children: [
+              Container(
+                color: Colors.white,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [Text('Heloo')],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [Text('Heloo')],
+                  ),
+                ),
+              ),
+            ])),
+      ),
+    );
+  }
+
+  Widget tabBar() {
+    return TabBar(
+      isScrollable: true,
+      controller: _controllerTab,
+      tabs: [
+        Tab(text: 'Overview'),
+        Tab(text: 'Resume'),
+      ],
+      indicatorColor: Colors.red,
+      indicatorSize: TabBarIndicatorSize.label,
+      labelColor: Colors.red,
+    );
+  }
+}
